1c5c3ed6d88fba24915cc28fae6659ac59890a3d
Garrett Maitland <gmaitlan18@gmail.com>
2025-08-15 14:31:28 -0400

    test: add test for encrypting punctuation

--- test/caesarcipher.test.js
+++ test/caesarcipher.test.js
@@ -3,3 +3,7 @@ const caesarCipher = require("../caesarcipher");
 test("HeLLo 3", () => {
   expect(caesarCipher.encrypt("HeLLo", 3)).toBe("KhOOr");
 });
+
+test("HeLLo World! 3", () => {
+  expect(caesarCipher.encrypt("HeLLo, World!", 3)).toBe("KhOOr, Zruog");
+});
